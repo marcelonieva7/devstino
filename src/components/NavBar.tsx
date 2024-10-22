fde99faec70bb929c891b5785e3683d6923d3b94
Marcelo Nieva <marcelonieva7@gmail.com>
2024-10-22 20:33:03 -0300

    show active navbar link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,21 +5,27 @@ import { ROUTES } from "@/enums";
 import clsx from "clsx";
 import Link from "next/link";
 import Audio from "@/components/Audio";
+import path from "path";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
-  const {userData, dataLoaded} = useUserDataContext()  
+  const {userData, dataLoaded} = useUserDataContext()
+  const pathname = usePathname()
   const TITLES = ["Home", "Crear", "Tu Devstino"] as const;
   const isDevstino = !!userData.devstinationSlug
+  const currentPath = path.dirname(pathname)
+  const devstino = `${ROUTES.DEVSTINO}/${userData.devstinationSlug}`
 
   return (
     <nav>
-      <ul className="text-zinc-300 flex justify-between gap-4 px-4 py-5 after:bg-zinc-800">
-        <Link className="text-xl hover:underline hover:text-red-700 transition-colors self-start" href={ROUTES.HOME}>{TITLES[0]}</Link>          
+      <ul className="text-zinc-300 flex justify-between gap-4 px-4 py-5 after:bg-zinc-800 underline-offset-4">
+        <Link className={clsx("text-xl hover:underline hover:text-red-700 transition-colors self-start", {"underline text-red-700": pathname === ROUTES.HOME})} href={ROUTES.HOME}>{TITLES[0]}</Link>          
         <Link
           className={clsx("text-xl hover:underline hover:text-red-700 transition-colors self-start", {
-            "bg-gray-600 rounded-lg inline-block w-40 animate-pulse text-gray-600 pointer-events-none": !dataLoaded
+            "bg-gray-600 rounded-lg inline-block w-40 animate-pulse text-gray-600 pointer-events-none": !dataLoaded,
+            "underline text-red-700": currentPath === path.dirname(ROUTES.ADD.STEP_ONE) || pathname === devstino
           })}
-          href={ isDevstino ? `${ROUTES.DEVSTINO}/${userData.devstinationSlug}` : ROUTES.ADD.STEP_ONE}
+          href={ isDevstino ? devstino : ROUTES.ADD.STEP_ONE}
         >
           {TITLES[isDevstino ? 2 : 1]}
         </Link>
