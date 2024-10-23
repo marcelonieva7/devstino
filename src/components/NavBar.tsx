f5d8e62d126f28162390aad367c882b0e58cf3fc
Marcelo Nieva <marcelonieva7@gmail.com>
2024-10-23 02:54:37 -0300

    fix minor issue

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -19,11 +19,11 @@ export default function NavBar() {
   return (
     <nav>
       <ul className="text-zinc-300 flex justify-between gap-4 px-4 py-5 after:bg-zinc-800 underline-offset-4">
-        <Link className={clsx("text-xl hover:underline hover:text-red-700 transition-colors self-start", {"underline text-red-700": pathname === ROUTES.HOME})} href={ROUTES.HOME}>{TITLES[0]}</Link>          
+        <Link className={clsx("text-xl hover:underline hover:text-red-500 transition-colors self-start", {"text-red-700": pathname === ROUTES.HOME})} href={ROUTES.HOME}>{TITLES[0]}</Link>          
         <Link
-          className={clsx("text-xl hover:underline hover:text-red-700 transition-colors self-start", {
+          className={clsx("text-xl hover:underline hover:text-red-500 transition-colors self-start", {
             "bg-gray-600 rounded-lg inline-block w-40 animate-pulse text-gray-600 pointer-events-none": !dataLoaded,
-            "underline text-red-700": currentPath === path.dirname(ROUTES.ADD.STEP_ONE) || pathname === devstino
+            "text-red-700": currentPath === path.dirname(ROUTES.ADD.STEP_ONE) || pathname === devstino
           })}
           href={ isDevstino ? devstino : ROUTES.ADD.STEP_ONE}
         >
