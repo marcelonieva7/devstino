40f4a87d0db0dbf66ee178a886daf6c47fc6ec7d
Marcelo Nieva <marcelonieva7@gmail.com>
2024-10-23 03:28:33 -0300

    add disabled to active links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,16 +15,30 @@ export default function NavBar() {
   const isDevstino = !!userData.devstinationSlug
   const currentPath = path.dirname(pathname)
   const devstino = `${ROUTES.DEVSTINO}/${userData.devstinationSlug}`
+  const isLink2Active = currentPath === path.dirname(ROUTES.ADD.STEP_ONE) || pathname === devstino
 
   return (
     <nav>
       <ul className="text-zinc-300 flex justify-between gap-4 px-4 py-5 after:bg-zinc-800 underline-offset-4">
-        <Link className={clsx("text-xl hover:underline hover:text-red-500 transition-colors self-start", {"text-red-700": pathname === ROUTES.HOME})} href={ROUTES.HOME}>{TITLES[0]}</Link>          
         <Link
-          className={clsx("text-xl hover:underline hover:text-red-500 transition-colors self-start", {
-            "bg-gray-600 rounded-lg inline-block w-40 animate-pulse text-gray-600 pointer-events-none": !dataLoaded,
-            "text-red-700": currentPath === path.dirname(ROUTES.ADD.STEP_ONE) || pathname === devstino
-          })}
+          className={clsx(
+            "text-xl hover:underline hover:text-red-500 transition-colors self-start",
+            {"text-red-700 pointer-events-none ": pathname === ROUTES.HOME}
+          )}
+          href={ROUTES.HOME}
+          aria-disabled={pathname === ROUTES.HOME}
+          tabIndex={pathname === ROUTES.HOME ? -1 : undefined}
+        >
+          {TITLES[0]}
+        </Link>
+        <Link
+          className={clsx(
+            "text-xl hover:underline hover:text-red-500 transition-colors self-start",
+            {"bg-gray-600 rounded-lg inline-block w-40 animate-pulse text-gray-600 pointer-events-none": !dataLoaded,
+            "text-red-700 pointer-events-none aria-current": isLink2Active}
+          )}
+          aria-disabled={!dataLoaded || !isLink2Active}
+          tabIndex={(!dataLoaded || !isLink2Active) ? -1 : undefined}
           href={ isDevstino ? devstino : ROUTES.ADD.STEP_ONE}
         >
           {TITLES[isDevstino ? 2 : 1]}
